270afd0e368c1a27c4a828d1d9f797a2cc056601
logancostello <locostel@calpoly.edu>
2025-07-27 23:05:30 -0700

    refreshed dist

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/PAT-Practice/', // 👈 Required for GitHub Pages!
+  base: '/PAT-Practice/', 
   plugins: [react()],
 })
